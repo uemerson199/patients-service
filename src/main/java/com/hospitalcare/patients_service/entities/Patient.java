034bdb57b4c7e7ed0ab4df32fd36b60b74ac4636
uemerson199 <uemersonribeiro220@gmail.com>
2025-09-06 11:15:18 -0300

    feat: Create Patient entity

---
+++ src/main/java/com/hospitalcare/patients_service/entities/Patient.java
@@ -0,0 +1,32 @@
+package com.hospitalcare.patients_service.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "patients")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDate dob;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+}
